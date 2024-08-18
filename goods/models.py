56ed8ede07d7aef34b51a7609ace89e2b0631015
Aigerim Sabyrkhan <aigerimsabyrkhan393@gmail.com>
2024-08-18 22:52:55 +0500

    PostgreSQL connected. full text search

--- goods/models.py
+++ goods/models.py
@@ -17,7 +17,7 @@ class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name="Название")
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
     description = models.TextField(blank = True, null=True, verbose_name="Описание")
-    image = models.ImageField(upload_to="good_images", blank = True, null = True, verbose_name="Изображение")
+    image = models.ImageField(upload_to="goods_images", blank = True, null = True, verbose_name="Изображение")
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена")
     discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="Скидка")
     quantity = models.PositiveIntegerField(default=0, verbose_name="Количсетво")
