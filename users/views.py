99af31f5f0128fcffbabc771473d28bb9acaad78
Aigerim Sabyrkhan <aigerimsabyrkhan393@gmail.com>
2024-08-31 22:58:40 +0500

    cart templates were included

--- users/views.py
+++ users/views.py
@@ -62,6 +62,17 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
+def users_cart(request):
+    return render(request, 'users/users_cart.html')
+
+
+
+
+
+
+
+
+
 @login_required
 
 def logout(request):
